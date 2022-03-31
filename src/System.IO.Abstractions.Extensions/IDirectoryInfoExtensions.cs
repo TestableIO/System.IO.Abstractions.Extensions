12cf11885deea3935e719f3c51a7d85763763f67
Martin <mayermart@users.noreply.github.com>
2022-03-31 12:35:39 +0200

    New Extension Method IDirectoryInfo.CopyTo
    Added an Extension Method to IDirectoryInfo to recursively copy a directory.

--- src/System.IO.Abstractions.Extensions/IDirectoryInfoExtensions.cs
+++ src/System.IO.Abstractions.Extensions/IDirectoryInfoExtensions.cs
@@ -3,25 +3,87 @@
     public static class IDirectoryInfoExtensions
     {
         /// <summary>
-        /// Get an <see cref="IDirectoryInfo"/> for the specified sub-directory <paramref name="name"/>
+        ///     Get an <see cref="IDirectoryInfo" /> for the specified Directory at <paramref name="destDirectoryName" />.
         /// </summary>
         /// <param name="info"></param>
-        /// <param name="name">Sub-directory name (ex. "test")</param>
-        /// <returns>An <see cref="IDirectoryInfo"/> for the specified sub-directory</returns>
-        public static IDirectoryInfo SubDirectory(this IDirectoryInfo info, string name)
+        /// <param name="destDirectoryName">Destination Path for the directory to be copied to.</param>
+        /// <returns>An <see cref="IDirectoryInfo" /> for the specified path.</returns>
+        public static IDirectoryInfo CopyTo(this IDirectoryInfo info, string destDirectoryName, bool recursive = false)
         {
-            return info.FileSystem.DirectoryInfo.FromDirectoryName(info.FileSystem.Path.Combine(info.FullName, name));
+            var dest = info.FileSystem.DirectoryInfo.FromDirectoryName(destDirectoryName);
+
+            return info.CopyTo(dest, recursive);
         }
 
         /// <summary>
-        /// Get an <see cref="IFileInfo"/> for the specified file <paramref name="name"/>
+        ///     Get an <see cref="IDirectoryInfo" /> for the specified Directory at <paramref name="destDirectoryName" />.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="destDirectory">Destination Path for the directory to be copied to.</param>
+        /// <returns>An <see cref="IDirectoryInfo" /> for the specified path.</returns>
+        public static IDirectoryInfo CopyTo(this IDirectoryInfo info, IDirectoryInfo destDirectory, bool recursive = false)
+        {
+            // Check if the source directory exists
+            info.Refresh();
+            if (!info.Exists)
+            {
+                throw new DirectoryNotFoundException($"Source directory not found: '{info.FullName}'");
+            }
+
+            destDirectory.Refresh();
+            if (destDirectory.Exists)
+            {
+                throw new IOException($"The directory '{destDirectory.FullName}' already exists.");
+            }
+
+            // Cache directories before we start copying
+            var subDirectories = info.GetDirectories();
+
+            // Create the destination directory
+            var fs = info.FileSystem;
+            fs.Directory.CreateDirectory(destDirectory.FullName);
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (var directoryInfo in subDirectories)
+                {
+                    var newDestDirectory = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(destDirectory.FullName, directoryInfo.Name));
+                    directoryInfo.CopyTo(newDestDirectory, true);
+                }
+            }
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (var fileInfo in info.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destDirectory.FullName, fileInfo.Name);
+                fileInfo.CopyTo(targetFilePath);
+            }
+
+            destDirectory.Refresh();
+            return destDirectory;
+        }
+
+        /// <summary>
+        ///     Get an <see cref="IFileInfo" /> for the specified file <paramref name="name" />
         /// </summary>
         /// <param name="info"></param>
         /// <param name="name">File name (ex. "test.txt")</param>
-        /// <returns>An <see cref="IFileInfo"/> for the specified file</returns>
+        /// <returns>An <see cref="IFileInfo" /> for the specified file</returns>
         public static IFileInfo File(this IDirectoryInfo info, string name)
         {
             return info.FileSystem.FileInfo.FromFileName(info.FileSystem.Path.Combine(info.FullName, name));
+        }
+
+        /// <summary>
+        ///     Get an <see cref="IDirectoryInfo" /> for the specified sub-directory <paramref name="name" />
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="name">Sub-directory name (ex. "test")</param>
+        /// <returns>An <see cref="IDirectoryInfo" /> for the specified sub-directory</returns>
+        public static IDirectoryInfo SubDirectory(this IDirectoryInfo info, string name)
+        {
+            return info.FileSystem.DirectoryInfo.FromDirectoryName(info.FileSystem.Path.Combine(info.FullName, name));
         }
     }
 }
--- tests/System.IO.Abstractions.Extensions.Tests/DirectoryInfoExtensionsTests.cs
+++ tests/System.IO.Abstractions.Extensions.Tests/DirectoryInfoExtensionsTests.cs
@@ -6,28 +6,230 @@ namespace System.IO.Abstractions.Tests
     public class DirectoryInfoExtensionsTests
     {
         [Test]
-        public void SubDirectory_Extension_Test()
+        public void CopyTo_NonRecursiveWithSubfolder_DoesNotCopySubfolder()
         {
             //arrange
             var fs = new FileSystem();
-            var current =  fs.DirectoryInfo.FromDirectoryName(fs.Directory.GetCurrentDirectory());
-            var guid = Guid.NewGuid().ToString();
-            var expectedPath = fs.Path.Combine(current.FullName, guid);
+            var workingDir = fs.DirectoryInfo.FromDirectoryName(fs.Directory.GetCurrentDirectory()).CreateSubdirectory(Guid.NewGuid().ToString());
 
-            //make sure directory doesn't exists
-            Assert.IsFalse(fs.Directory.Exists(expectedPath));
+            //create directories
+            var source = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "SourceDir"));
+            var sourceSubDir = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "SourceDir", "SubDir"));
+            var dest = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "DestDir"));
 
-            //create directory
-            var created = current.SubDirectory(guid);
-            created.Create();
+            source.Create();
+            sourceSubDir.Create();
 
-            //assert it exists
-            Assert.IsTrue(fs.Directory.Exists(expectedPath));
-            Assert.AreEqual(expectedPath, created.FullName);
+            //make sure everything is set up as expected
+            Assert.IsTrue(fs.Directory.Exists(source.FullName));
+            Assert.IsTrue(fs.Directory.Exists(sourceSubDir.FullName));
+            Assert.IsFalse(fs.Directory.Exists(dest.FullName));
 
-            //delete directory
-            created.Delete();
-            Assert.IsFalse(fs.Directory.Exists(expectedPath));
+            //act
+            var newDest = source.CopyTo(dest.FullName);
+
+            var destSubDir = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(newDest.FullName, "SubDir"));
+            Assert.IsTrue(fs.Directory.Exists(newDest.FullName));
+            Assert.IsFalse(fs.Directory.Exists(destSubDir.FullName));
+
+            //delete file
+            source.Delete(recursive: true);
+            dest.Delete(recursive: true);
+
+            Assert.IsFalse(fs.File.Exists(source.FullName));
+            Assert.IsFalse(fs.File.Exists(dest.FullName));
+        }
+
+        [Test]
+        public void CopyTo_NonRecursiveWithSubfolderWithFiles_DoesNotCopySubfolderAndSubfolderFiles()
+        {
+            //arrange
+            var fs = new FileSystem();
+            var workingDir = fs.DirectoryInfo.FromDirectoryName(fs.Directory.GetCurrentDirectory()).CreateSubdirectory(Guid.NewGuid().ToString());
+
+            //create directories
+            var source = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "SourceDir"));
+            var sourceSubDir = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "SourceDir", "SubDir"));
+            var dest = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "DestDir"));
+
+            source.Create();
+            sourceSubDir.Create();
+
+            //create files
+            var sourceFile = fs.FileInfo.FromFileName(fs.Path.Combine(source.FullName, "file.txt"));
+            var sourceSubDirFile = fs.FileInfo.FromFileName(fs.Path.Combine(sourceSubDir.FullName, "file.txt"));
+
+            using (var _ = sourceFile.Create())
+            {
+            }
+
+            using (var _ = sourceSubDirFile.Create())
+            {
+            }
+
+            //make sure everything is set up as expected
+            Assert.IsTrue(fs.Directory.Exists(source.FullName));
+            Assert.IsTrue(fs.Directory.Exists(sourceSubDir.FullName));
+            Assert.IsTrue(fs.File.Exists(sourceFile.FullName));
+            Assert.IsTrue(fs.File.Exists(sourceSubDirFile.FullName));
+            Assert.IsFalse(fs.Directory.Exists(dest.FullName));
+
+            //act
+            var newDest = source.CopyTo(dest.FullName);
+
+            var destFile = fs.FileInfo.FromFileName(fs.Path.Combine(newDest.FullName, "file.txt"));
+            var destSubDir = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(newDest.FullName, "SubDir"));
+            var destSubDirFile = fs.FileInfo.FromFileName(fs.Path.Combine(destSubDir.FullName, "file.txt"));
+            Assert.IsTrue(fs.Directory.Exists(newDest.FullName));
+            Assert.IsTrue(fs.File.Exists(destFile.FullName));
+            Assert.IsFalse(fs.Directory.Exists(destSubDir.FullName));
+            Assert.IsFalse(fs.File.Exists(destSubDirFile.FullName));
+
+            //delete file
+            source.Delete(recursive: true);
+            dest.Delete(recursive: true);
+
+            Assert.IsFalse(fs.File.Exists(source.FullName));
+            Assert.IsFalse(fs.File.Exists(dest.FullName));
+        }
+
+        [Test]
+        public void CopyTo_RecursiveWithSubfolder_DoesCopySubfolder()
+        {
+            //arrange
+            var fs = new FileSystem();
+            var workingDir = fs.DirectoryInfo.FromDirectoryName(fs.Directory.GetCurrentDirectory()).CreateSubdirectory(Guid.NewGuid().ToString());
+
+            //create directories
+            var source = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "SourceDir"));
+            var sourceSubDir = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(source.FullName, "SubDir"));
+            var dest = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "DestDir"));
+
+            source.Create();
+            sourceSubDir.Create();
+
+            //make sure everything is set up as expected
+            Assert.IsTrue(fs.Directory.Exists(source.FullName));
+            Assert.IsTrue(fs.Directory.Exists(sourceSubDir.FullName));
+            Assert.IsFalse(fs.Directory.Exists(dest.FullName));
+
+            //act
+            var newDest = source.CopyTo(dest.FullName, recursive: true);
+
+            var destSubDir = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(newDest.FullName, "SubDir"));
+            Assert.IsTrue(fs.Directory.Exists(newDest.FullName));
+            Assert.IsTrue(fs.Directory.Exists(destSubDir.FullName));
+
+            //delete file
+            source.Delete(recursive: true);
+            dest.Delete(recursive: true);
+
+            Assert.IsFalse(fs.File.Exists(source.FullName));
+            Assert.IsFalse(fs.File.Exists(dest.FullName));
+        }
+
+        [Test]
+        public void CopyTo_RecursiveWithSubfolderWithFiles_DoesCopySubfolderAndSubfolderFiles()
+        {
+            //arrange
+            var fs = new FileSystem();
+            var workingDir = fs.DirectoryInfo.FromDirectoryName(fs.Directory.GetCurrentDirectory()).CreateSubdirectory(Guid.NewGuid().ToString());
+
+            //create directories
+            var source = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "SourceDir"));
+            var sourceSubDir = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(source.FullName, "SubDir"));
+            var dest = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "DestDir"));
+
+            source.Create();
+            sourceSubDir.Create();
+
+            //create files
+            var sourceFile = fs.FileInfo.FromFileName(fs.Path.Combine(source.FullName, "file.txt"));
+            var sourceSubDirFile = fs.FileInfo.FromFileName(fs.Path.Combine(sourceSubDir.FullName, "file.txt"));
+
+            using (var _ = sourceFile.Create())
+            {
+            }
+
+            using (var _ = sourceSubDirFile.Create())
+            {
+            }
+
+            //make sure everything is set up as expected
+            Assert.IsTrue(fs.Directory.Exists(source.FullName));
+            Assert.IsTrue(fs.Directory.Exists(sourceSubDir.FullName));
+            Assert.IsTrue(fs.File.Exists(sourceFile.FullName));
+            Assert.IsTrue(fs.File.Exists(sourceSubDirFile.FullName));
+            Assert.IsFalse(fs.Directory.Exists(dest.FullName));
+
+            //act
+            var newDest = source.CopyTo(dest.FullName, recursive: true);
+
+            var destFile = fs.FileInfo.FromFileName(fs.Path.Combine(newDest.FullName, "file.txt"));
+            var destSubDir = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(newDest.FullName, "SubDir"));
+            var destSubDirFile = fs.FileInfo.FromFileName(fs.Path.Combine(destSubDir.FullName, "file.txt"));
+            Assert.IsTrue(fs.Directory.Exists(newDest.FullName));
+            Assert.IsTrue(fs.File.Exists(destFile.FullName));
+            Assert.IsTrue(fs.Directory.Exists(destSubDir.FullName));
+            Assert.IsTrue(fs.File.Exists(destSubDirFile.FullName));
+
+            //delete file
+            source.Delete(recursive: true);
+            dest.Delete(recursive: true);
+
+            Assert.IsFalse(fs.File.Exists(source.FullName));
+            Assert.IsFalse(fs.File.Exists(dest.FullName));
+        }
+
+        [Test]
+        public void CopyTo_SourceDirDoesNotExists_ThrowsDirectoryNotFoundException()
+        {
+            //arrange
+            var fs = new FileSystem();
+            var workingDir = fs.DirectoryInfo.FromDirectoryName(fs.Directory.GetCurrentDirectory()).CreateSubdirectory(Guid.NewGuid().ToString());
+
+            //create directories
+            var source = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "SourceDir"));
+            var dest = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "DestDir"));
+
+            //make sure everything is set up as expected
+            Assert.IsFalse(fs.Directory.Exists(source.FullName));
+            Assert.IsFalse(fs.Directory.Exists(dest.FullName));
+
+            //act
+            Assert.That(() => source.CopyTo(dest.FullName), Throws.Exception.TypeOf<DirectoryNotFoundException>().And.Message.EqualTo($"Source directory not found: '{source.FullName}'"));
+
+            Assert.IsFalse(fs.File.Exists(source.FullName));
+            Assert.IsFalse(fs.File.Exists(dest.FullName));
+        }
+
+        [Test]
+        public void CopyTo_TargetDirExists_ThrowsIOException()
+        {
+            //arrange
+            var fs = new FileSystem();
+            var workingDir = fs.DirectoryInfo.FromDirectoryName(fs.Directory.GetCurrentDirectory()).CreateSubdirectory(Guid.NewGuid().ToString());
+
+            //create directories
+            var source = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "SourceDir"));
+            var dest = fs.DirectoryInfo.FromDirectoryName(fs.Path.Combine(workingDir.FullName, "DestDir"));
+
+            source.Create();
+            dest.Create();
+
+            //make sure everything is set up as expected
+            Assert.IsTrue(fs.Directory.Exists(source.FullName));
+            Assert.IsTrue(fs.Directory.Exists(dest.FullName));
+
+            //act
+            Assert.That(() => source.CopyTo(dest.FullName), Throws.Exception.TypeOf<IOException>().And.Message.EqualTo($"The directory '{dest.FullName}' already exists."));
+
+            //delete file
+            source.Delete();
+            dest.Delete();
+
+            Assert.IsFalse(fs.File.Exists(source.FullName));
+            Assert.IsFalse(fs.File.Exists(dest.FullName));
         }
 
         [Test]
@@ -56,6 +258,31 @@ namespace System.IO.Abstractions.Tests
             //delete file
             created.Delete();
             Assert.IsFalse(fs.File.Exists(expectedPath));
+        }
+
+        [Test]
+        public void SubDirectory_Extension_Test()
+        {
+            //arrange
+            var fs = new FileSystem();
+            var current = fs.DirectoryInfo.FromDirectoryName(fs.Directory.GetCurrentDirectory());
+            var guid = Guid.NewGuid().ToString();
+            var expectedPath = fs.Path.Combine(current.FullName, guid);
+
+            //make sure directory doesn't exists
+            Assert.IsFalse(fs.Directory.Exists(expectedPath));
+
+            //create directory
+            var created = current.SubDirectory(guid);
+            created.Create();
+
+            //assert it exists
+            Assert.IsTrue(fs.Directory.Exists(expectedPath));
+            Assert.AreEqual(expectedPath, created.FullName);
+
+            //delete directory
+            created.Delete();
+            Assert.IsFalse(fs.Directory.Exists(expectedPath));
         }
     }
 }
