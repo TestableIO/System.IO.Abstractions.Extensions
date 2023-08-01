ee441df22bdd69574b39b5e4d977dbb682ea3f20
Matt Kotsenas <matt.kotsenas@gmail.com>
2023-07-31 17:17:17 -0700

    Add DisposableFile and unit tests

---
+++ src/System.IO.Abstractions.Extensions/DisposableFile.cs
@@ -0,0 +1,57 @@
+﻿namespace System.IO.Abstractions.Extensions
+{
+    /// <summary>
+    /// Creates a file that will be deleted when the <see cref="Dispose()"/> method is called.
+    /// </summary>
+    /// <remarks>
+    /// This class is designed for the <c>using</c> pattern to ensure that a file is
+    /// created and then deleted when it is no longer referenced. This is sometimes called
+    /// the RAII pattern (see https://en.wikipedia.org/wiki/Resource_acquisition_is_initialization).
+    /// </remarks>
+    public class DisposableFile : IDisposable
+    {
+        private bool isDisposed;
+        private readonly IFileInfo fileInfo;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DisposableFile"/> class.
+        /// </summary>
+        /// <param name="fileInfo">
+        /// The file to delete when this object is disposed.
+        /// </param>
+        public DisposableFile(IFileInfo fileInfo)
+        {
+            this.fileInfo = fileInfo ?? throw new ArgumentNullException(nameof(fileInfo));
+        }
+
+        /// <summary>
+        /// Performs the actual work of releasing resources. This allows for subclasses to participate
+        /// in resource release.
+        /// </summary>
+        /// <param name="disposing">
+        /// <c>true</c> if if the call comes from a <see cref="Dispose()"/> method, <c>false</c> if it
+        /// comes from a finalizer.
+        /// </param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    fileInfo.Delete();
+                }
+
+                isDisposed = true;
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method.
+            // See https://learn.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-dispose.
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ tests/System.IO.Abstractions.Extensions.Tests/DisposableFileTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace System.IO.Abstractions.Extensions.Tests
+{
+    [TestFixture]
+    public class DisposableFileTests
+    {
+        [Test]
+        public void DisposableFile_Throws_ArgumentNullException_For_Null_IFileInfo_Test()
+        {
+            Assert.Throws<ArgumentNullException>(() => new DisposableFile(null!));
+        }
+
+        [Test]
+        public void DisposableFile_Delete_On_Dispose_Test()
+        {
+            // Arrange
+            var fs = new FileSystem();
+            var path = fs.Path.Combine(fs.Directory.GetCurrentDirectory(), fs.Path.GetRandomFileName());
+            var fileInfo = fs.FileInfo.New(path);
+            fileInfo.Create().Dispose();
+
+            // Assert file exists
+            Assert.IsTrue(fs.File.Exists(path), "File exists");
+
+            // Act
+            var disposableFile = new DisposableFile(fileInfo);
+            disposableFile.Dispose();
+
+            // Assert directory is deleted
+            Assert.IsFalse(fs.File.Exists(path), "File does not exist");
+
+            // Assert a second dispose does not throw
+            Assert.DoesNotThrow(() => disposableFile.Dispose());
+        }
+    }
+}
