ef025f1681e27eb4cf73415476228492c564b995
Matt Kotsenas <matt.kotsenas@gmail.com>
2023-07-31 22:18:35 -0700

    Add CreateDisposableFile extension method

--- src/System.IO.Abstractions.Extensions/IFileSystemExtensions.cs
+++ src/System.IO.Abstractions.Extensions/IFileSystemExtensions.cs
@@ -57,13 +57,7 @@
 
             if (directoryInfo.Exists)
             {
-                // Having the colliding path availabe as part of the exception is very useful for debugging.
-                // However, paths can be considered sensitive information in some contexts (like web servers).
-                // Thus, we add the path to the exception's data , rather than the message.
-                var ex = new ArgumentException("Directory already exists", nameof(path));
-                ex.Data.Add("path", path);
-
-                throw ex;
+                throw CreateAlreadyExistsException(nameof(path), path);
             }
 
             directoryInfo.Create();
@@ -73,6 +67,76 @@
             directoryInfo.Refresh();
 
             return new DisposableDirectory(directoryInfo);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="DisposableFile"/> using a random name from the temp path.
+        /// </summary>
+        /// <param name="fileSystem">
+        /// The <see cref="IFileSystem"/> in use.
+        /// </param>
+        /// <param name="fileInfo">
+        /// The <see cref="IFileInfo"/> for the file that was created.
+        /// </param>
+        /// <returns>
+        /// A <see cref="DisposableFile"/> to manage the file's lifetime.
+        /// </returns>
+        public static DisposableFile CreateDisposableFile(this IFileSystem fileSystem, out IFileInfo fileInfo)
+        {
+            var temp = fileSystem.Path.GetTempPath();
+            var fileName = fileSystem.Path.GetRandomFileName();
+            var path = fileSystem.Path.Combine(temp, fileName);
+
+            return fileSystem.CreateDisposableFile(path, out fileInfo);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="DisposableFile"/> using the provided <paramref name="path"/>.
+        /// </summary>
+        /// <param name="fileSystem">
+        /// The <see cref="IFileSystem"/> in use.
+        /// </param>
+        /// <param name="path">
+        /// The full path to the file to create.
+        /// </param>
+        /// <param name="fileInfo">
+        /// The <see cref="IFileInfo"/> for the file that was craeted.
+        /// </param>
+        /// <returns>
+        /// A <see cref="DisposableFile"/> to manage the file's lifetime.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// If the file already exists.
+        /// </exception>
+        public static DisposableFile CreateDisposableFile(this IFileSystem fileSystem, string path, out IFileInfo fileInfo)
+        {
+            fileInfo = fileSystem.FileInfo.New(path);
+
+            if (fileInfo.Exists)
+            {
+                throw CreateAlreadyExistsException(nameof(path), path);
+            }
+
+            // Ensure we close the handle to the file after we create it, otherwise
+            // callers may get an access denied error.
+            fileInfo.Create().Dispose();
+
+            // Do an attribute refresh so that the object we return to the caller
+            // has up-to-date properties (like Exists).
+            fileInfo.Refresh();
+
+            return new DisposableFile(fileInfo);
+        }
+
+        private static ArgumentException CreateAlreadyExistsException(string argumentName, string path)
+        {
+            // Having the colliding path availabe as part of the exception is very useful for debugging.
+            // However, paths can be considered sensitive information in some contexts (like web servers).
+            // Thus, we add the path to the exception's data , rather than the message.
+            var ex = new ArgumentException("File already exists", argumentName);
+            ex.Data.Add("path", path);
+
+            return ex;
         }
     }
 }
--- tests/System.IO.Abstractions.Extensions.Tests/FileSystemExtensionsTests.cs
+++ tests/System.IO.Abstractions.Extensions.Tests/FileSystemExtensionsTests.cs
@@ -53,5 +53,44 @@ namespace System.IO.Abstractions.Extensions.Tests
             fs.Directory.Delete(path);
             Assert.IsFalse(fs.Directory.Exists(path), "Directory should not exist");
         }
+
+        [Test]
+        public void CreateDisposableFile_Temp_Path_Test()
+        {
+            // Arrange
+            var fs = new FileSystem();
+            string path;
+
+            // Act
+            using (_ = fs.CreateDisposableFile(out var file))
+            {
+                path = file.FullName;
+
+                Assert.IsTrue(file.Exists, "File should exist");
+                Assert.IsTrue(
+                    path.StartsWith(fs.Path.GetTempPath(), StringComparison.Ordinal),
+                    "File should be in temp path");
+            }
+
+            // Assert file is deleted
+            Assert.IsFalse(fs.File.Exists(path), "File should not exist");
+        }
+
+        [Test]
+        public void CreateDisposableFile_Already_Exists_Test()
+        {
+            // Arrange
+            var fs = new FileSystem();
+            var path = fs.Path.Combine(fs.Path.GetTempPath(), fs.Path.GetRandomFileName());
+            fs.File.Create(path).Dispose();
+
+            // Assert
+            var ex = Assert.Throws<ArgumentException>(() => fs.CreateDisposableFile(path, out _));
+            Assert.True(ex.Data["path"].ToString() == path, "Exception data should contain colliding path to aid with debugging");
+
+            // Delete colliding file
+            fs.File.Delete(path);
+            Assert.IsFalse(fs.File.Exists(path), "File should not exist");
+        }
     }
 }
