32cf7162cd6836c1699cef26696cef48daf63a29
Matt Kotsenas <matt.kotsenas@gmail.com>
2023-07-31 21:40:54 -0700

    Add CreateDisposableDirectory extension method

--- src/System.IO.Abstractions.Extensions/IFileSystemExtensions.cs
+++ src/System.IO.Abstractions.Extensions/IFileSystemExtensions.cs
@@ -11,5 +11,68 @@
         {
             return fileSystem.DirectoryInfo.New(fileSystem.Directory.GetCurrentDirectory());
         }
+
+        /// <summary>
+        /// Creates a new <see cref="DisposableDirectory"/> using a random name from the temp path.
+        /// </summary>
+        /// <param name="fileSystem">
+        /// The <see cref="IFileSystem"/> in use.
+        /// </param>
+        /// <param name="directoryInfo">
+        /// The <see cref="IDirectoryInfo"/> for the directory that was created.
+        /// </param>
+        /// <returns>
+        /// A <see cref="DisposableDirectory"/> to manage the directory's lifetime.
+        /// </returns>
+        public static DisposableDirectory CreateDisposableDirectory(this IFileSystem fileSystem, out IDirectoryInfo directoryInfo)
+        {
+            var temp = fileSystem.Path.GetTempPath();
+            var fileName = fileSystem.Path.GetRandomFileName();
+            var path = fileSystem.Path.Combine(temp, fileName);
+
+            return fileSystem.CreateDisposableDirectory(path, out directoryInfo);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="DisposableDirectory"/> using the provided <paramref name="path"/>.
+        /// </summary>
+        /// <param name="fileSystem">
+        /// The <see cref="IFileSystem"/> in use.
+        /// </param>
+        /// <param name="path">
+        /// The full path to the directory to create.
+        /// </param>
+        /// <param name="directoryInfo">
+        /// The <see cref="IDirectoryInfo"/> for the directory that was craeted.
+        /// </param>
+        /// <returns>
+        /// A <see cref="DisposableDirectory"/> to manage the directory's lifetime.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// If the directory already exists.
+        /// </exception>
+        public static DisposableDirectory CreateDisposableDirectory(this IFileSystem fileSystem, string path, out IDirectoryInfo directoryInfo)
+        {
+            directoryInfo = fileSystem.DirectoryInfo.New(path);
+
+            if (directoryInfo.Exists)
+            {
+                // Having the colliding path availabe as part of the exception is very useful for debugging.
+                // However, paths can be considered sensitive information in some contexts (like web servers).
+                // Thus, we add the path to the exception's data , rather than the message.
+                var ex = new ArgumentException("Directory already exists", nameof(path));
+                ex.Data.Add("path", path);
+
+                throw ex;
+            }
+
+            directoryInfo.Create();
+
+            // Do an attribute refresh so that the object we return to the caller
+            // has up-to-date properties (like Exists).
+            directoryInfo.Refresh();
+
+            return new DisposableDirectory(directoryInfo);
+        }
     }
 }
--- tests/System.IO.Abstractions.Extensions.Tests/FileSystemExtensionsTests.cs
+++ tests/System.IO.Abstractions.Extensions.Tests/FileSystemExtensionsTests.cs
@@ -14,5 +14,44 @@ namespace System.IO.Abstractions.Extensions.Tests
             Assert.IsFalse(String.IsNullOrWhiteSpace(fullName));
             Assert.AreEqual(Environment.CurrentDirectory, fullName);
         }
+
+        [Test]
+        public void CreateDisposableDirectory_Temp_Path_Test()
+        {
+            // Arrange
+            var fs = new FileSystem();
+            string path;
+
+            // Act
+            using (_ = fs.CreateDisposableDirectory(out var dir))
+            {
+                path = dir.FullName;
+
+                Assert.IsTrue(dir.Exists, "Directory should exist");
+                Assert.IsTrue(
+                    path.StartsWith(fs.Path.GetTempPath(), StringComparison.Ordinal),
+                    "Directory should be in temp path");
+            }
+
+            // Assert directory is deleted
+            Assert.IsFalse(fs.Directory.Exists(path), "Directory should not exist");
+        }
+
+        [Test]
+        public void CreateDisposableDirectory_Already_Exists_Test()
+        {
+            // Arrange
+            var fs = new FileSystem();
+            var path = fs.Path.Combine(fs.Path.GetTempPath(), fs.Path.GetRandomFileName());
+            fs.Directory.CreateDirectory(path);
+
+            // Assert
+            var ex = Assert.Throws<ArgumentException>(() => fs.CreateDisposableDirectory(path, out _));
+            Assert.True(ex.Data["path"].ToString() == path, "Exception data should contain colliding path to aid with debugging");
+
+            // Delete colliding directory
+            fs.Directory.Delete(path);
+            Assert.IsFalse(fs.Directory.Exists(path), "Directory should not exist");
+        }
     }
 }
