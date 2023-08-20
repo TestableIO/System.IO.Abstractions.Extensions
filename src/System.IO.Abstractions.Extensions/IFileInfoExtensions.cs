19864c86290c878a5e254197385426fc5442131c
Luigi Grilli <luigi.grilli@gmail.com>
2023-08-20 21:03:05 +0200

    feat: Add EnumerateLines extension method

--- src/System.IO.Abstractions.Extensions/IFileInfoExtensions.cs
+++ src/System.IO.Abstractions.Extensions/IFileInfoExtensions.cs
@@ -1,4 +1,8 @@
-﻿namespace System.IO.Abstractions
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.IO.Abstractions
 {
     public static class IFileInfoExtensions
     {
@@ -11,6 +15,16 @@
         {
             if (!info.Exists)
                 throw new FileNotFoundException(StringResources.Format("COULD_NOT_FIND_FILE_EXCEPTION", info.FullName));
+        }
+
+        public static IEnumerable<string> EnumerateLines(this IFileInfo info)
+        {
+            return new LineEnumerable(info, null);
+        }
+
+        public static IEnumerable<string> EnumerateLines(this IFileInfo info, Encoding encoding)
+        {
+            return new LineEnumerable(info, encoding);
         }
     }
 }
---
+++ src/System.IO.Abstractions.Extensions/LineEnumerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.IO.Abstractions
+{
+    internal sealed class LineEnumerable : IEnumerable<string>
+    {
+        private readonly IFileInfo _file;
+        private readonly Encoding _encoding;
+
+        public LineEnumerable(IFileInfo file, Encoding encoding)
+        {
+            _file = file;
+            _encoding = encoding;
+        }
+
+        public IEnumerator<string> GetEnumerator() => new LineEnumerator(_file, _encoding);
+
+        IEnumerator IEnumerable.GetEnumerator() => new LineEnumerator(_file, _encoding);
+    }
+
+    internal sealed class LineEnumerator : IEnumerator<string>
+    {
+        private Stream _stream;
+        private StreamReader _reader;
+        private string _current;
+
+        public LineEnumerator(IFileInfo file, Encoding encoding)
+        {
+            _stream = file.OpenRead();
+            _reader = encoding == null
+                ? new StreamReader(_stream)
+                : new StreamReader(_stream, encoding);
+        }
+
+        public string Current => _current;
+
+        object IEnumerator.Current => _current;
+
+        public void Dispose()
+        {
+            _reader?.Dispose();
+            _reader = null;
+            _stream?.Dispose();
+            _stream = null;
+        }
+
+        public bool MoveNext()
+        {
+            _current = _reader.ReadLine();
+            return _current != null;
+        }
+
+        public void Reset()
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
