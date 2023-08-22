d6ade14a52d02ee7df5e64ac2cab057369680e91
Luigi Grilli <luigi.grilli@gmail.com>
2023-08-22 12:18:20 +0200

    Added more tests

--- tests/System.IO.Abstractions.Extensions.Tests/FileInfoExtensionsTests.cs
+++ tests/System.IO.Abstractions.Extensions.Tests/FileInfoExtensionsTests.cs
@@ -189,5 +189,80 @@ namespace System.IO.Abstractions.Extensions.Tests
                 Assert.AreEqual(content[i], actual[i]);
             }
         }
+
+        [TestCase("line1", "line2", "line3")]
+        [TestCase("line1", "", "line3")]
+        public void WriteLinesWithUTF16Encoding_WriteLinesToNewFile_LinesAreWritten(params string[] content)
+        {
+            //arrange
+            var fs = new FileSystem();
+            var current = fs.DirectoryInfo.New(fs.Directory.GetCurrentDirectory());
+            var guid = Guid.NewGuid().ToString();
+            var file = current.File(guid);
+
+            //act
+            Assert.IsFalse(file.Exists);
+            file.WriteLines(content, Encoding.Unicode);
+            var actual = file.EnumerateLines(Encoding.Unicode).ToArray();
+
+            //assert
+            Assert.AreEqual(content.Length, actual.Length);
+            for (int i = 0; i < content.Length; i++)
+            {
+                Assert.AreEqual(content[i], actual[i]);
+            }
+        }
+
+        [TestCase("line1", "line2", "line3")]
+        [TestCase("line1", "", "line3")]
+        public void WriteLinesWithUTF16Encoding_WriteLinesToExistingFileWithOverwriteDisabled_ThrowsIOException(params string[] content)
+        {
+            //arrange
+            var fs = new FileSystem();
+            var current = fs.DirectoryInfo.New(fs.Directory.GetCurrentDirectory());
+            var guid = Guid.NewGuid().ToString();
+            var file = current.File(guid);
+            file.Truncate();
+
+            //act & assert
+            Assert.IsTrue(file.Exists);
+            //call WriteLines with both overwrite parameter ommitted or set to false
+            var ex1 = Assert.Throws<IOException>(() => file.WriteLines(content, Encoding.Unicode));
+            var ex2 = Assert.Throws<IOException>(() => file.WriteLines(content, Encoding.Unicode, false));
+
+            Assert.IsTrue(ex1.Message.Contains(file.FullName));
+            Assert.IsTrue(ex2.Message.Contains(file.FullName));
+        }
+
+        [TestCase("line1", "line2", "line3")]
+        [TestCase("line1", "", "line3")]
+        public void WriteLinesWithUTF16Encoding_WriteLinesToExistingFileWithOverwriteEnabled_FileIsTruncatedAndLinesAreWritten(params string[] content)
+        {
+            //arrange
+            var fs = new FileSystem();
+            var current = fs.DirectoryInfo.New(fs.Directory.GetCurrentDirectory());
+            var guid = Guid.NewGuid().ToString();
+            var file = current.File(guid);
+
+            //create file with long content
+            var data = Encoding.Unicode.GetBytes("line5 line4 line3 line2 line1");
+            using (var stream = file.OpenWrite())
+            {
+                stream.Write(data, 0, data.Length);
+                stream.Dispose();
+            }
+
+            //act
+            Assert.IsTrue(file.Exists);
+            file.WriteLines(content, Encoding.Unicode, overwrite: true);
+            var actual = file.EnumerateLines(Encoding.Unicode).ToArray();
+
+            //assert
+            Assert.AreEqual(content.Length, actual.Length);
+            for (int i = 0; i < content.Length; i++)
+            {
+                Assert.AreEqual(content[i], actual[i]);
+            }
+        }
     }
 }
